dab2c43ecb9f22e48dfc90a791c914be50c0ba42
Casey Hyde <hyde.casey@gmail.com>
2016-11-21 13:51:25 -0600

    forgot to add routes folder on last commit

---
+++ server/routes/treats.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var connectionString = "postgres://localhost:5432/sigma";
+
+router.get('/', function(req, res) {
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log("Could not connect to the database to get treats");
+      res.sendStatus(500);
+    }
+    client.query('SELECT * FROM treats', function(err, result) {
+      done();
+      if(err) {
+        console.log("Query error getting treats");
+        res.sendStatus(500);
+      }
+      res.send(result.rows);
+    });//end query
+  });//end connect
+});//end route
+
+router.post('/', function(req, res) {
+  var treat = req.body;
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log("Could not connect to database to post new treat");
+      res.sendStatus(500);
+    }
+    client.query('INSERT INTO treats (name, description, pic) VALUES ($1, $2, $3)',
+    [treat.name, treat.description, treat.url], function(err, result) {
+      done();
+      if(err) {
+        console.log("Query error adding new treat to database");
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(201);
+      }
+    });//end query
+  });//end connect
+});//end route
+
+module.exports = router;
